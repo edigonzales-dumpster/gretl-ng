252a40c2407a1ec61d7d78545c838a180aaef124
edigonzales <edi.gonzales@gmail.com>
2021-07-01 11:44:57 +0200

    use local maven

--- src/main/java/ch/so/agi/gretl/HelloWorld.java
+++ src/main/java/ch/so/agi/gretl/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld extends Task {
     }
 
     public void execute() {
-        System.out.println("Hello World");            
+        System.out.println("Hello World Fubar");            
 
         // use of the reference to Project-instance
         //String message = getProject().getProperty("ant.project.name");
