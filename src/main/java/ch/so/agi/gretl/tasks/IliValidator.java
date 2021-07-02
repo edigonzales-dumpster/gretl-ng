f669012589548e21afc12be9d2260b793cb095a6
edigonzales <edi.gonzales@gmail.com>
2021-07-02 13:40:25 +0200

    add gretl.properties

--- src/main/java/ch/so/agi/gretl/tasks/IliValidator.java
+++ src/main/java/ch/so/agi/gretl/tasks/IliValidator.java
@@ -20,7 +20,7 @@ public class IliValidator extends AbstractValidatorTask {
         Settings settings = new Settings();
         
         
-        
+        System.out.println("foo");
 
     }
     
